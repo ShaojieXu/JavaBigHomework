8a484e55f650b3a3a07c787bdebe742f47590a48
shengdao <211471317@qq.com>
2022-04-11 12:29:38 +0800

    version 1.0

---
+++ src/MyHomework/Main.java
@@ -0,0 +1,8 @@
+package MyHomework;
+
+public class Main {
+    public static void main(String[] args) {
+        Windows w = new Windows();
+        w.init();
+    }
+}
---
+++ src/MyHomework/Windows.java
@@ -0,0 +1,14 @@
+package MyHomework;
+
+import javax.swing.*;
+
+public class Windows {
+    JFrame jf;
+
+    void init() {
+        jf = new JFrame("最小代价生成树算法演示");
+        jf.setVisible(true);
+        jf.setBounds(200, 100, 1000, 600);
+    }
+}
+
